3ac27a625f8e61f40565287ff3d684e450d55d37
Ahmed Dikha <85714557+bestwall2@users.noreply.github.com>
2023-06-17 18:50:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const http = require('http');
 const PORT = 3000;
+const { Client } = require('whatsapp-web.js');
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+
+  const client = new Client();
+
+client.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+
+client.initialize();
   res.end('Hello World!');
 });
 
